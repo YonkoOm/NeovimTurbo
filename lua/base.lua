18f33452f13af5a75f9a010bda6b06b6a67350d9
Om Mordia <moradiaom1@gmail.com>
2022-09-05 23:06:19 -0500

    changed map leader to space instead of backslash

--- lua/base.lua
+++ lua/base.lua
@@ -5,6 +5,9 @@ vim.opt.encoding = 'utf-8'
 vim.opt.fileencoding = 'utf-8'
 
 vim.wo.number = true
+vim.wo.relativenumber = true
+
+vim.g.mapleader = " "
 
 vim.opt.title = true
 vim.opt.autoindent = true
