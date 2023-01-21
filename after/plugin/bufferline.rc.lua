d3400303535f4767064d3b7c12d31367d55da013
Om Mordia <moradiaom1@gmail.com>
2023-01-21 15:25:38 -0600

    added separator padding and made bufferline colors more kanagawa focused

--- after/plugin/bufferline.rc.lua
+++ after/plugin/bufferline.rc.lua
@@ -1,45 +1,44 @@
 local status, bufferline = pcall(require, "bufferline")
-if (not status) then return end
-local status, tokyonight = pcall(require, "tokyonight.colors")
-if (not status) then return end
-local colors = tokyonight.setup()
+if not status then
+	return
+end
+local color_status, kanagawa = pcall(require, "kanagawa.colors")
+if not color_status then
+	return
+end
+local colors = kanagawa.setup()
 
 bufferline.setup({
-  options = {
-    mode = "tabs",
-    separator_style = 'slant',
-    always_show_bufferline = true,
-    show_buffer_close_icons = false,
-    show_close_icon = false,
-    color_icons = true
-  },
-  highlights = {
-    separator = {
-      -- fg = '#073642',
-      fg = colors.border_highlight,
-      -- bg = '#002b36',
-      bg = colors.bg_dark
-    },
-    separator_selected = {
-      -- fg = '#073642',
-      fg = colors.border_highlight,
-    },
-    background = {
-      fg = '#657b83',
-      -- bg = '#002b36'
-      bg = colors.bg_dark
-    },
-    buffer_selected = {
-      fg = '#fdf6e3',
-      bold = true,
-      italic = false,
-    },
-    fill = {
-      -- bg = '#073642'
-      bg = colors.bg_statusline
-    }
-  },
+	options = {
+		mode = "tabs",
+		separator_style = "padded_slant",
+		always_show_bufferline = true,
+		show_buffer_close_icons = false,
+		show_close_icon = false,
+		color_icons = true,
+	},
+	highlights = {
+		separator = {
+			fg = colors.crystalBlue,
+			bg = colors.sumiInk0,
+		},
+		separator_selected = {
+			fg = colors.crystalBlue,
+		},
+		background = {
+			fg = "#657b83",
+			bg = colors.sumiInk0,
+		},
+		buffer_selected = {
+			fg = "#fdf6e3",
+			bold = true,
+			italic = false,
+		},
+		fill = {
+			bg = colors.sumiInk0,
+		},
+	},
 })
 
-vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', {})
-vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', {})
+vim.keymap.set("n", "<Tab>", "<Cmd>BufferLineCycleNext<CR>", {})
+vim.keymap.set("n", "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", {})
