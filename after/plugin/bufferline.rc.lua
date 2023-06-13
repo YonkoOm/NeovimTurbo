b8e529ec13547769e6147a818a7a039fe329ec12
Om Mordia <moradiaom1@gmail.com>
2023-06-13 17:15:55 -0500

    changed separator from padded_slant to slant

--- after/plugin/bufferline.rc.lua
+++ after/plugin/bufferline.rc.lua
@@ -11,7 +11,7 @@ local colors = kanagawa.setup({ theme = "wave" }).palette
 bufferline.setup({
 	options = {
 		mode = "tabs",
-		separator_style = "padded_slant",
+		separator_style = "slant",
 		always_show_bufferline = true,
 		show_buffer_close_icons = false,
 		show_close_icon = false,
