c134a222f8ea2494d9e29111d5fb325c6d8d5187
Om Mordia <moradiaom1@gmail.com>
2024-05-16 20:04:00 -0500

    refactor and added new keymappings

--- lua/yonkoom/plugins/lsp/lspconfig.lua
+++ lua/yonkoom/plugins/lsp/lspconfig.lua
@@ -23,22 +23,18 @@ return {
 		-- Use an on_attach function to only map the following keys
 		-- after the language server attaches to the current buffer
 		local on_attach = function(client, bufnr)
-			local function buf_set_keymap(...)
-				vim.api.nvim_buf_set_keymap(bufnr, ...)
-			end
-
-			--local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-			--Enable completion triggered by <c-x><c-o>
-			--buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+			local keymap = vim.keymap
 
 			-- Mappings.
 			local opts = { noremap = true, silent = true }
 
 			-- See `:help vim.lsp.*` for documentation on any of the below functions
-			buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-			--buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-			buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-			--buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+			keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+			keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+			-- keymap.set("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+
+			opts.desc = "Restart LSP"
+			keymap.set("n", "<leader>rs", "<Cmd>LspRestart<CR>", opts) -- mapping to restart lsp if necessary
 		end
 
 		-- Set up completion using nvim_cmp with LSP source
