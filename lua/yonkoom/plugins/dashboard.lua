1ceb2a5500f0a57f6f8c579f1257950441eb7c4b
Om Mordia <moradiaom1@gmail.com>
2024-05-27 19:17:29 -0500

    removed unnecessary curly brackets

--- lua/yonkoom/plugins/dashboard.lua
+++ lua/yonkoom/plugins/dashboard.lua
@@ -1,6 +1,6 @@
 return {
 	"nvimdev/dashboard-nvim",
-	dependencies = { { "nvim-tree/nvim-web-devicons" } },
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 	event = "VimEnter",
 	config = function()
 		require("dashboard").setup({
