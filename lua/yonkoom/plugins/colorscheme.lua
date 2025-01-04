72a55aea01a43b0502f8ada9c5c3706a0e42c0eb
Om Mordia <moradiaom1@gmail.com>
2025-01-04 14:54:19 -0600

    tint the background of diagnostic messages with their foreground color

--- lua/yonkoom/plugins/colorscheme.lua
+++ lua/yonkoom/plugins/colorscheme.lua
@@ -18,6 +18,10 @@ return {
 			overrides = function(colors)
 				local theme = colors.theme
 				local palette = colors.palette
+				local makeDiagnosticColor = function(color)
+					local c = require("kanagawa.lib.color")
+					return { fg = color, bg = c(color):blend(theme.ui.bg, 0.95):to_hex() }
+				end
 				return {
 					CursorLine = { bg = palette.winterBlue },
 					CursorLineNr = { fg = palette.lightBlue, bold = false },
@@ -37,6 +41,11 @@ return {
 					PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
 					PmenuSbar = { bg = theme.ui.bg_m1 },
 					PmenuThumb = { bg = theme.ui.bg_p2 },
+
+					DiagnosticVirtualTextHint = makeDiagnosticColor(theme.diag.hint),
+					DiagnosticVirtualTextInfo = makeDiagnosticColor(theme.diag.info),
+					DiagnosticVirtualTextWarn = makeDiagnosticColor(theme.diag.warning),
+					DiagnosticVirtualTextError = makeDiagnosticColor(theme.diag.error),
 				}
 			end,
 		})
