00a1cd097c5ff7c57e9b87d7be7ba39d50ebea73
Om Mordia <moradiaom1@gmail.com>
2025-01-08 13:45:04 -0600

    updated keymaps to allow for quickly switching between files

--- lua/yonkoom/plugins/harpoon.lua
+++ lua/yonkoom/plugins/harpoon.lua
@@ -6,9 +6,11 @@ return {
 
 		vim.keymap.set("n", "<leader>a", mark.add_file)
 		vim.keymap.set("n", "<leader>e", ui.toggle_quick_menu)
+		vim.keymap.set("n", "<C-h>", ui.nav_prev)
+		vim.keymap.set("n", "<C-l>", ui.nav_next)
 
 		for i = 1, 5 do
-			vim.keymap.set("n", string.format("<leader>%s", i), function()
+			vim.keymap.set("n", string.format("<C-%s>", i), function()
 				ui.nav_file(i)
 			end)
 		end
